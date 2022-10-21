50e6c02a37846d3dc20ac39136f678a9c9fb4273
alexandremarchand <alexandremarchand@10.3.60.184>
2022-10-21 16:16:22 +0200

    Création d'un nouveau projet versionné"

---
+++ com.mycompany_TP2_Bieres_MARCHAND_jar_1.0-SNAPSHOT/src/main/java/BouteilleBiere.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alexandremarchand
+ */
+public class BouteilleBiere {
+    String Nom;
+    float degreAlcool;
+    String brasserie;
+    boolean ouverte;
+}
---
+++ com.mycompany_TP2_Bieres_MARCHAND_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/tp2_bieres_marchand/TP2_Bieres_MARCHAND.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp2_bieres_marchand;
+
+/**
+ *
+ * @author alexandremarchand
+ */
+public class TP2_Bieres_MARCHAND {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
