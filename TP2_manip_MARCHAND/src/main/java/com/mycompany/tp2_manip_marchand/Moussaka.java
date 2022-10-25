088703f8c4e7b1fc8c239c018b3c49dc53b8a0f0
alexandremarchand <alexandremarchand@10.3.60.184>
2022-10-25 12:57:36 +0200

    le programme fonctionne

---
+++ TP2_manip_MARCHAND/src/main/java/com/mycompany/tp2_manip_marchand/Moussaka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2_manip_marchand;
+
+/**
+ *
+ * @author alexandremarchand
+ */
+public class Moussaka {
+  int nbCalories;
+   public Moussaka ( int Cal) {  // constructeur permettant d'initialiser l'attribut NbCalories pour une valeur entrée en paramètre
+              Moussaka =Cal;
+    
+}
---
+++ TP2_manip_MARCHAND/src/main/java/com/mycompany/tp2_manip_marchand/TP2_manip_MARCHAND.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp2_manip_marchand;
+
+/**
+ *
+ * @author alexandremarchand
+ */
+public class TP2_manip_MARCHAND {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        
+        
+    }
+}
