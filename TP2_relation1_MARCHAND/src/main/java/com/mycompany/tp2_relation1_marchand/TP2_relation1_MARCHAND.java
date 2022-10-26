c9030f710b4ae66dba2e43402af58ab00b3507df
alexandremarchand <alexandremarchand@10.3.60.184>
2022-10-27 00:27:47 +0200

    On a le code testé en 3. du sujet de tp qui s'exécute bien sans erreur

--- TP2_relation1_MARCHAND/src/main/java/com/mycompany/tp2_relation1_marchand/TP2_relation1_MARCHAND.java
+++ TP2_relation1_MARCHAND/src/main/java/com/mycompany/tp2_relation1_marchand/TP2_relation1_MARCHAND.java
@@ -13,5 +13,11 @@ public class TP2_relation1_MARCHAND {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ; Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ; Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ; Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+Personne bob = new Personne("Bobby", "Sixkiller");
+Personne reno = new Personne("Reno", "Raines");
+System.out.println("liste des voitures disponibles "+ uneClio + "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+   
     }
 }
